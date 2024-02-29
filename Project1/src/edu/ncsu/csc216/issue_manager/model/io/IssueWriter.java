386503aca91a96ebbc79cdcede47210433c7c873
Louis Ton <thton@ncsu.edu>
2024-02-29 10:13:11 -0500

    [Implement] IssueWriter class

--- Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueWriter.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueWriter.java
@@ -1,5 +1,8 @@
 package edu.ncsu.csc216.issue_manager.model.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 
 import edu.ncsu.csc216.issue_manager.model.issue.Issue;
@@ -21,7 +24,13 @@ public class IssueWriter {
 	 * @param filename the file to write into
 	 * @param issues the list of issues
 	 */
-	public static void writeIssuesToFile(String filename, List<Issue> issues) {
-		
+	public static void writeIssuesToFile(String filename, List<Issue> issues) throws IOException {
+		PrintStream fileWriter = new PrintStream(new File(filename));
+
+		for (int i = 0; i < issues.size(); i++) {
+			fileWriter.println(issues.get(i).toString());
+		}
+
+		fileWriter.close();
 	}
 }
