d825d5b1b226241713f789776ad2845626e08491
Louis Ton <thton@ncsu.edu>
2024-02-29 10:10:27 -0500

    [Implement] IssueReader class

--- Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueReader.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueReader.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc216.issue_manager.model.io;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.issue_manager.model.issue.Issue;
 
@@ -22,7 +23,21 @@ public class IssueReader {
 	 * @return an array of Issues
 	 */
 	public static ArrayList<Issue> readIssuesFromFile(String filename) {
-		return null;
+		ArrayList<Issue> issueList = new ArrayList<Issue>();
+		Scanner scan = new Scanner(filename);
+		try {
+			scan.useDelimiter("\\r?\\n?[*]");
+			while(scan.hasNext()) {
+				String issueString = scan.next();
+				Issue issue = processIssue(issueString);
+				issueList.add(issue);
+			}
+			scan.close();
+		} catch (IllegalArgumentException e){
+			//Allow exception to propagate to the IssueReader caller
+		}
+		
+		return issueList;
 	}
 	
 	/**
@@ -31,6 +46,34 @@ public class IssueReader {
 	 * @return an issue
 	 */
 	private static Issue processIssue(String line) {
-		return null;
+		Scanner scanner = new Scanner(line);
+		scanner.useDelimiter(",");
+		try {
+			int id = scanner.nextInt();
+			String state = scanner.next();
+			String type = scanner.next();
+			String summary =  scanner.next();
+			String owner = scanner.next();
+			boolean confirmed = scanner.nextBoolean();
+			String resolution = scanner.next();
+			String notes = scanner.next();
+			
+			ArrayList<String> notesArray = new ArrayList<String>();
+			Scanner forNote = new Scanner(notes);
+			forNote.useDelimiter("\\r?\\n?[-]");
+			while(forNote.hasNext()) {
+				String note = forNote.next();
+				if(!note.isEmpty()) {
+					notesArray.add(note);
+				}
+			}
+			forNote.close();
+			scanner.close();
+			Issue i = new Issue(id, state, type, summary, owner, confirmed, resolution, notesArray);
+			return i;
+			
+		} catch (Exception e) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
